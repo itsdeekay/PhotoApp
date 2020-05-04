4e08968e0d7b334c395e64377de889953e9a8dd5
Deepak Kumar <deepjindl65@gmail.com>
2020-05-05 01:20:10 +0530

    single repos

---
+++ client/build/precache-manifest.6ce19088c2948155e6870ba924712c47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51253e5c09afe072a4fe057476097be2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4c1eae366a9e929d57e",
+    "url": "/static/css/main.2b73df20.chunk.css"
+  },
+  {
+    "revision": "b5df9b48cee9a4c776c9",
+    "url": "/static/js/2.84f8f0fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84f8f0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4c1eae366a9e929d57e",
+    "url": "/static/js/main.95d5a63d.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
